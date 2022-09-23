2ab1a8f0dd0fed22f8f6c13f6fb08429e0fed3ca
Crowdbotics <engineering@crowdbotics.com>
2022-09-23 15:07:25 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/devops_chatto_9_dev_35080/MainActivity.java
@@ -0,0 +1,15 @@
+package com.devops_chatto_9_dev_35080;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "devops_chatto_9_dev_35080";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "devops_chatto_9_dev_35080",
+  url: "https://devops_chatto_9_dev_35080.botics.co",
+  api: "https://devops_chatto_9_dev_35080.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
